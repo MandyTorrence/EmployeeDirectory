713933fa613d382a05da5bcce43ec2443ea7234d
Mandy Torrence <mandolinhaynes@gmail.com>
2020-04-27 02:20:08 -0400

    Header Component and setting up the style sheet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import EmployeeTable from './components/EmployeeTable';
+import "./styles/style.css";
 
 import './App.css';
 
